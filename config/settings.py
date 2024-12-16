6f2916fac9d807460579a4e44aa7e0a97ae05166
yusufyassir <yusufyassir0@gmail.com>
2024-12-16 14:25:51 +0200

    heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)d8@+hyshtyk1%@f8ywz4w*)m5t1&-41e6h$_w)vlucu30*12w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
